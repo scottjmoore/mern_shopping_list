5005f166e6db0e35e3f46aadd0dae8062b37c834
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-03 03:31:54 +0100

    Add application navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/appNavbar';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <AppNavbar />
     )
   }
 };
