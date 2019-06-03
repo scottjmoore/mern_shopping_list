2412970bf2d57eb958ec94a0390fc06e6a15dcbf
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-03 14:05:24 +0100

    Change port to  or 3000 if not set

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ mongoose.connect(db, { useNewUrlParser: true }).then( () => console.log('MongoDB
 
 app.use('/api/items', items);
 
-const port = 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
