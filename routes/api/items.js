9c0f49e961ea79a7c15b097dae1e3b3ab8fc78c5
Scott Moore <scott.moore@scottmooresoftware.com>
2019-06-03 01:51:00 +0100

    Fix formatting

--- routes/api/items.js
+++ routes/api/items.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Item = require('../../models/Item');
 
 router.get('/', (req, res) => {
-    Item.find().sort({ date: -1 }).then( (items) => res.json(items));
+    Item.find().sort({ date: -1 }).then(items => res.json(items));
 });
 
 router.post('/', (req, res) => {
